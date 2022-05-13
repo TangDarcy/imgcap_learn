c1163bdb2eff55839ae960ae6e03a7da7b1811da
TangDarcy <1771428957@qq.com>
2022-05-13 22:21:32 +0800

    增加注册接口

--- imgcap_flask/app.py
+++ imgcap_flask/app.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify
 from user import User
+from flask_cors import *
 
 app = Flask(__name__)
 
+CORS(app,supports_credentials = True)
 """
 接口说明：
 1.返回json数据
@@ -13,13 +15,31 @@ app = Flask(__name__)
     message:"对本次请求的说明"
 }
 """
+app.config['JSON_AS_ASCII'] = False #解决中文乱码
 
+login_code_dic={-1:"用户不存在",0:"登录成功",1:"密码错误"}
 
-@app.route('/')
-def hello_world():
+# @app.route('/')
+# def hello_world():
+#     user=User()
+#     arrData=user.get_user_info()
+#     userName="Tang"
+#     passWord="tang123"
+#     # userName="Qian"
+#     login_code=user.is_login(userName,passWord)
+#     print(login_code_dic[login_code])
+#     return jsonify(login_code_dic[login_code])
+
+@app.route('/login',methods=['POST'])
+def user_login():
+    data=request.get_json(silent=True)
+    userName=data["userName"]
+    passWord=data["passWord"]
     user=User()
-    arrData=user.get_user_info()
-    return jsonify(arrData)
+    login_code=user.is_login(userName,passWord)
+    return jsonify(login_code_dic[login_code])
+
+
 
 
 if __name__=="__main__":
--- imgcap_flask/user.py
+++ imgcap_flask/user.py
@@ -1,6 +1,7 @@
 from pymysql import connect
 from pymysql.cursors import DictCursor
 from settings import MYHOST,MYPORT,MYUSER,MYPASSWORD,MYDATABASE
+
 class User(object):
     def __init__(self):
         self.conn= connect(
@@ -23,6 +24,21 @@ class User(object):
         
         data=[]
         for temp in self.cursor.fetchall():
-            data.append(temp)
-        
+            data.append(temp)  
         return data
+    
+    def is_login(self,userName,passWord):#-1代表用户名不存在，0表示登录成功，1表示密码错误
+        print(userName)
+        sql="""select username,password from i_user where username='{}'""".format(userName) 
+        self.cursor.execute(sql)
+        data=[]
+        for temp in self.cursor.fetchall():
+            data.append(temp)
+        if len(data)==0:
+            return -1
+        elif data[0]["password"]==passWord:
+            return 0
+        else:
+             return 1
+        return 1
+
